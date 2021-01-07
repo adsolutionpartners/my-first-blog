bd19104ce45b769f852fbb0b3337765430d4c39f
June <Jun@example.com>
2021-01-07 12:27:27 +0900

    add new file

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,13 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
-    auther = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True,null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -15,4 +16,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
